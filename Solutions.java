77737f03eefc94dc51ad4fc4666d4ae06ef880c0
client2 <client2@email.com>
2017-10-18 22:54:44 +0800

    Edited a line

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,5 @@
 public class Solutions{
   public static void main(String[] args){
-    System.out.println("Here are the solutions.");
+    System.out.println("Here are the solutions. OKAY");
   }
 }
