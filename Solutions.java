2a2c3e37f64c19486446dd54471e166b5c24d05e
kasima <krmmendoza@slu.edu.ph>
2017-10-20 08:42:01 +0800

    Changed return text to a greeting.

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,5 @@
 public class Solutions{
   public static void main(String[] args){
-    System.out.println("Edit");
+    System.out.println("Hello everybody! I hope you are enjoying this.");
   }
 }
