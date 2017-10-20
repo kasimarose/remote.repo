34742b640bd42dfa2b0404ebb528cea6214d2767
kasimarose <krmmendoza@slu.edu.ph>
2017-10-20 09:55:07 +0800

    Changed greetings to Hi 9:30

--- Solutions.java
+++ Solutions.java
@@ -1,5 +1,5 @@
 public class Solutions{
   public static void main(String[] args){
-    System.out.println("Hello everybody! I hope you are enjoying this.");
+    System.out.println("Hi 9:30 class!.");
   }
 }
